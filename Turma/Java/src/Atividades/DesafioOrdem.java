82295a2836f2675992bb87a9baa878fd92b64ef1
Ingrid Aquino <ingridaquinosoares@live.com>
2022-07-09 23:18:26 -0300

    :construction: Desafio add

---
+++ Turma/Java/src/Atividades/DesafioOrdem.java
@@ -0,0 +1,64 @@
+package Atividades;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DesafioOrdem {
+    public static void main(String[] args) {
+        desafio();
+    }
+
+    public static void desafio(){
+        Scanner entrada = new Scanner(System.in);
+
+        int a, b, c;
+
+
+        int[] nums = new int[3];
+
+        do {
+
+            System.out.println("Digite um número: ");
+            a = entrada.nextInt();
+
+            System.out.println("Digite outro número: ");
+            b = entrada.nextInt();
+
+            System.out.println("Digite mais um número: ");
+            c = entrada.nextInt();
+
+
+            if (a < b && a < c) {
+                nums[0] = a;
+            } else if (a < b || a < c) {
+                nums[1] = a;
+            } else {
+                nums[2] = a;
+            }
+
+
+            if (b < a && b < c) {
+                nums[0] = b;
+            } else if (b < a || b < c) {
+                nums[1] = b;
+            } else {
+                nums[2] = b;
+            }
+
+            if (c < a && c < b) {
+                nums[0] = c;
+            } else if (c < a || c < b) {
+                nums[1] = c;
+            } else {
+                nums[2] = c;
+            }
+
+
+            System.out.println("\nOrdem Crescente: ");
+            System.out.println(Arrays.toString(nums) + "\n");
+
+        }while (a != 0 || b != 0 || c != 0 );
+
+
+    }
+}
--- Turma/Java/src/Atividades/OrdemCrescente.java
+++ Turma/Java/src/Atividades/OrdemCrescente.java
@@ -11,36 +11,48 @@ public class OrdemCrescente {
     public static void ordem() {
         Scanner entrada = new Scanner(System.in);
 
-
-        int aux, num1, num2, num3;
+        int[] nums = new int[3];
+        int a, b, c;
 
         System.out.println("Digite um número: ");
-        num1 = entrada.nextInt();
+        a = entrada.nextInt();
 
         System.out.println("Digite outro número: ");
-        num2 = entrada.nextInt();
+        b = entrada.nextInt();
 
         System.out.println("Digite mais um número: ");
-        num3 = entrada.nextInt();
+        c = entrada.nextInt();
 
-        if( num1 > num2) {
-            aux = num1;
-            num1 = num2;
-            num2 = aux;
-        } else if (num2 > num3) {
-            aux = num2;
-            num2 = num3;
-            num3 = aux;
-        }else {
-            aux = num3;
-            num3 = num1;
-            num1 = aux;
+
+        if (a < b && a < c) {
+            nums[0] = a;
+        } else if (a < b || a < c) {
+            nums[1] = a;
+        } else {
+            nums[2] = a;
         }
 
-        System.out.println("Ordem Crescente: ");
-        System.out.println(num1 + " " + num2 + " " + num3 + " ");
+
+            if (b < a && b < c) {
+                nums[0] = b;
+            } else if (b < a || b < c) {
+                nums[1] = b;
+            } else {
+                nums[2] = b;
+            }
+
+            if (c < a && c < b) {
+                nums[0] = c;
+            } else if (c < a || c < b) {
+                nums[1] = c;
+            } else {
+                nums[2] = c;
+            }
+
+            System.out.println("Ordem Crescente: ");
+            System.out.println(Arrays.toString(nums));
 
 
-    entrada.close();
+            entrada.close();
+        }
     }
-}
